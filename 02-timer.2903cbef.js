1526beffe5eac92d6b13a476bf1ac9c96093e4e7
healer5 <olenka.derevianko@gmail.com>
2022-10-22 06:46:00 +0000

    Deploying to gh-pages from @ healer5/goit-js-hw-09@4f8901313fc6fc0e7dd1455bbf41f918103fa6db 🚀

--- 02-timer.2903cbef.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=Date.now();setInterval((function(){var o=Date.now(),t=getTimeComponents(o-n),c=t.hours,e=t.mins,a=t.secs;console.log("".concat(c,":").concat(e,":").concat(a))}),1e3)}();
-//# sourceMappingURL=02-timer.2903cbef.js.map
