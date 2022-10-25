fdad4e34613a15cdb424514ddc3a0548c7b75be2
healer5 <olenka.derevianko@gmail.com>
2022-10-25 09:37:54 +0000

    Deploying to gh-pages from @ healer5/goit-js-hw-09@0dd8febc96c3b212e309e2becc3e2e26b2bd823a 🚀

--- 03-promises.a400586e.js
+++
@@ -1,2 +0,0 @@
-let i=null;const t={notification:document.querySelector(".js-alert")};function e(){t.notification.classList.remove("is-visible")}t.notification.addEventListener("click",(function(){e(),clearTimeout(i)})),t.notification.classList.add("is-visible"),i=setTimeout((()=>{e()}),3e3);
-//# sourceMappingURL=03-promises.a400586e.js.map
